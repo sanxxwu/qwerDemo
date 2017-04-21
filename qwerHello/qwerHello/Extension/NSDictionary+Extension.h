c80f5dc93289189e7d027fb8e54a77c6fd1924f4
王先强 <251411092@qq.com>
2017-04-21 11:36:09 +0800

    21231

---
+++ qwerHello/qwerHello/Extension/NSDictionary+Extension.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Extension.h
+//  qwerHello
+//
+//  Created by sw on 2017/4/21.
+//  Copyright © 2017年 sw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Extension)
+
+@end
---
+++ qwerHello/qwerHello/Extension/NSDictionary+Extension.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Extension.m
+//  qwerHello
+//
+//  Created by sw on 2017/4/21.
+//  Copyright © 2017年 sw. All rights reserved.
+//
+
+#import "NSDictionary+Extension.h"
+
+@implementation NSDictionary (Extension)
+
+@end
